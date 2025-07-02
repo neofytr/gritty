336030f8731801837a43289745d418c5b53e6627
neofytr <imagination12344321.com@gmail.com>
2025-07-03 00:39:55 +0530

    somewhat ready

--- include/shapes.h
+++ include/shapes.h
@@ -8,9 +8,39 @@
 #define BWT_MAX_Y 25
 
 // 16 colors supported (from 0 to 15)
+/*
+
+In DOS 16-color graphics modes (like CGA/EGA/VGA 16-color modes), the colors are typically based on a 4-bit color value (from 0 to 15), where each bit corresponds to a specific RGB intensity:
+
+Bit 0: Blue
+
+Bit 1: Green
+
+Bit 2: Red
+
+Bit 3: Intensity (bright)
+
+*/
 #define SCG_MODE 0x12
 #define SCG_MAX_X 640
 #define SCG_MAX_Y 480
+
+#define COLOR_BLACK 0
+#define COLOR_BLUE 1
+#define COLOR_GREEN 2
+#define COLOR_CYAN 3
+#define COLOR_RED 4
+#define COLOR_MAGENTA 5
+#define COLOR_BROWN 6
+#define COLOR_LIGHT_GRAY 7
+#define COLOR_DARK_GRAY 8
+#define COLOR_LIGHT_BLUE 9
+#define COLOR_LIGHT_GREEN 10
+#define COLOR_LIGHT_CYAN 11
+#define COLOR_LIGHT_RED 12
+#define COLOR_LIGHT_MAGENTA 13
+#define COLOR_YELLOW 14
+#define COLOR_WHITE 15
 
 #define MAX_MODE 0x13
 
@@ -18,7 +48,7 @@
 typedef struct packed
 {
     uint16_t x, y;
-    uint8_t color : 4; // our color pallette supports c0 to cfff (16 colors), so we need only 4 bits
+    uint8_t color;
 } point_t;
 
 // the color field does nothing in the B/W Text mode
@@ -26,7 +56,7 @@ typedef struct packed
 {
     point_t *one, *two;
     uint16_t thickness;
-    uint8_t color : 4;
+    uint8_t color;
 } line_t;
 
 // color field is ignored for B/W text mode
